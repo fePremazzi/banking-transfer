ad27532b5ef38bdb5a4b1b39d4af2c1a5fe16568
Fellipe Rego <felliperego@hotmail.com>
2020-07-20 22:07:10 -0300

    transfer equals and hashcode

--- src/main/java/com/cvc/banking_transfer/domain/model/Transfer.java
+++ src/main/java/com/cvc/banking_transfer/domain/model/Transfer.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -22,6 +24,7 @@ public class Transfer {
 	private String destinationAccount;
 	private BigDecimal value;
 	
+	@Enumerated(EnumType.STRING)
 	private TaxType taxType;
 	private BigDecimal taxValue;
 	
@@ -75,6 +78,28 @@ public class Transfer {
 		this.scheduledDate = scheduledDate;
 	}
 	
-	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Transfer other = (Transfer) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 	
 }
